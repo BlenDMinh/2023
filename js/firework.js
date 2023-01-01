8ecd331fd362cdbddd083b265b5c13883975f679
thuanvoviet <34815358+thuanvoviet@users.noreply.github.com>
2023-01-01 23:18:42 +0700

    small changes

--- js/firework.js
+++ js/firework.js
@@ -20,7 +20,7 @@ const RGB2Hex = function(rgb) {
 function Particle(pos, vel, color=undefined) {
   this.pos = pos;
   this.vel = vel;
-  this.acc = new THREE.Vector3(0, -1, 0)
+  this.acc = new THREE.Vector3(0, -.001, 0)
   this.color = color
   if (this.color == undefined)
     this.color = HSLToRGB(THREE.Math.randInt(0, 360), 100, 100)
@@ -46,27 +46,26 @@ function Firework(pos, size=undefined, color=undefined) {
   this.pos = pos.clone();
   this.size = size;
   if (size == undefined)
-    this.size = size = THREE.Math.randInt(30, 70)
+    this.size = size = THREE.Math.randInt(50, 70)
   this.particles = []
   for (let i = 0; i < this.size; ++i) {
     let phi = THREE.Math.randFloat(0, 2*Math.PI)
     let theta = THREE.Math.randFloat(0, Math.PI)
     // console.log(phi, theta)
-    let r = Math.sqrt(size)
+    let r = Math.sqrt(size)/100
     let x = r*Math.cos(phi)*Math.sin(theta)
-    let y = r*Math.sin(phi)*Math.sin(theta)
-    let z = r*Math.cos(theta)
+    let z = r*Math.sin(phi)*Math.sin(theta)
+    let y = r*Math.cos(theta)
     this.particles.push(new Particle(this.pos, new THREE.Vector3(x, y, z)))
   }
-  this.ttl = this.size*5;
+  this.ttl = this.size*3;
   this.lifetime = 0;
-  console.log(this)
+  // console.log(this)
 }
 
 Firework.prototype.update = function() {
-  if(this.lifetime > this.ttl) {
+  if(this.lifetime > this.ttl)
     this.particles.forEach(particle => scene.remove(particle.particleObj));
-  }
   else
     this.particles.forEach(particles => particles.update())
 }
