83919ad9bb7f298e152b8b84085a6c13c5713ceb
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2023-01-02 12:58:34 +0700

    #

--- js/audio.js
+++ js/audio.js
@@ -5,12 +5,12 @@ function PlayAudio(audioPath) {
 }
 
 export function PlayLofi() {
-    var audio = PlayAudio('audio/Pillars-Of-Creation.mp3');
+    var audio = PlayAudio('./audio/Pillars-Of-Creation.mp3');
     audio.loop = true;
 }
 
 export function PlayFirework() {
     var i = Math.floor(Math.random() * 3 + 1);
-    var audioPath = "audio/firework" + i + ".mp3";
+    var audioPath = "./audio/firework" + i + ".mp3";
     PlayAudio(audioPath);
 }
